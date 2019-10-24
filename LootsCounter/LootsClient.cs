674e3dec0aa6acacc57f6c4de69f518b600e6de6
Jordy <jorstar@localhost.localdomain>
2019-10-24 16:14:08 +0000

    updates to twitchlib

---
+++ LootsCounter/LootsClient.cs
@@ -0,0 +1,42 @@
+﻿using LootsCounter.Controllers;
+using LootsCounter.Controllers.Loots;
+using LootsCounter.Controllers.Twitch;
+using LootsCounter.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LootsCounter
+{
+    class LootsClient
+    {
+        public static LootsClient ActiveInstance { get; set; }
+        internal Cache Cache { get; }
+        internal Settings Settings { get; }
+        internal ChatBot ChatBot { get; }
+        internal Commands Commands { get; }
+        internal Counter Counter { get; }
+        internal Message MessageHelper { get; }
+
+        public LootsClient() {
+            Cache = new Cache( this );
+            Thread.Sleep( 1 );
+            Settings = new Settings( this );
+            Thread.Sleep( 1 );
+            ChatBot = new ChatBot( this );
+            Thread.Sleep( 1 );
+            Commands = new Commands( this );
+            Thread.Sleep( 1 );
+            Counter = new Counter( this );
+            Thread.Sleep( 1 );
+            MessageHelper = new Message( this );
+
+            ActiveInstance = this;
+
+            Commands.PrepareCommands();
+        }
+    }
+}
---
+++ LootsCounter/LootsClientAccessor.cs
@@ -0,0 +1,11 @@
+﻿namespace LootsCounter
+{
+    internal class LootsClientAccessor
+    {
+        protected LootsClient LootsClient { get; private set; }
+
+        protected LootsClientAccessor( LootsClient lootsClient ) {
+            LootsClient = lootsClient;
+        }
+    }
+}
